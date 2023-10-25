2affe742e2b7282afbf78082558c2bd9df0feadb
Aluno <aluno@pedagogico.ceupi.edu.br>
2023-10-25 10:13:01 -0300

    push

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 //adicionado interação ao botão de tema escuro:
-document.getElementById("dark").onclick = dark = () =>{
+    dark = () => { 
     const dark = document.getElementById("corpo")
     dark.style.background = "#001a1a" 
+    const caixa = document.getElementById("caixa")
+    caixa.style.background = "#001a1a" 
 }
+// Adicionando evento a função dark:
+document.getElementById("dark").addEventListener("click",dark)
 
-//adicionado interação ao botão de tema claro:
-document.getElementById("clean").onclick = () => {
-    const clean = document.getElementById("corpo")
-    clean.style.background = "#b3ffff"
+//adicionado interação a função de tema claro:
+clean = () => {
+    const clean =  document.getElementById("corpo")
+    clean.style.background = "#80ffd4"
+    const caixa = document.getElementById("caixa")
+    caixa.style.background = "#80ffd4"
 }
+// Adicionando evento a função clean:
+document.getElementById("clean").addEventListener("click", clean)
 
 //Interação de voltar ao tema normal da pag:
-document.getElementById("normal").onclick = noraml = () => {
-    const normal = document.getElementById("corpo")
+normal = () => {
+   const normal = document.getElementById("corpo")
     normal.style.background = "white"
+    const caixa = document.getElementById("caixa")
+    caixa.style.background = "white"
 }
+// Adicionado evento a função normal:
+document.getElementById("normal").addEventListener("click" , normal)
+
+
